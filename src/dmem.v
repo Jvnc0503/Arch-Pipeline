09df2022d2cec469454f15a534ed235d2447629e
Jvnc0503 <jvnc0503@gmail.com>
2026-06-29 18:09:43 -0500

    feat: Initializa memory

--- src/dmem.v
+++ src/dmem.v
@@ -4,6 +4,12 @@ module dmem(
     output wire [31:0] rd
 );
     reg [31:0] RAM[63:0];
+    integer i;
+    initial begin
+        for (i = 0; i < 64; i = i + 1) begin
+            RAM[i] = 32'd0;
+        end
+    end
     assign rd = RAM[a[31:2]]; 
     always @(posedge clk) begin
         if (we) RAM[a[31:2]] <= wd;
